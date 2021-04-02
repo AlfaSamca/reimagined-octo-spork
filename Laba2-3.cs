c8f5b30fc41e56e9067f6cf0a0033ba4ea3f8e46
AlfaSamca <79315532+AlfaSamca@users.noreply.github.com>
2021-04-02 14:52:29 +0300

    Add files via upload

---
+++ Laba2-3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _2LB___3ZDN
+{
+    class Laba2_3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A=");
+            long first = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("B=");
+            long last = Convert.ToInt64(Console.ReadLine());
+            long counter = 0;
+            if (first % 2 == 1) first++;
+            for (long i = first; i <= last; i += 2)
+                for (long j = i; j % 2 == 0; j /= 2, counter++) ;
+            Console.WriteLine("Maks=" + counter);
+            Console.ReadLine();
+        }
+    }
+}

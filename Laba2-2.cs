c5d8730f67e8378ce74a214b4aeff6781298bba8
AlfaSamca <79315532+AlfaSamca@users.noreply.github.com>
2021-04-02 14:50:47 +0300

    Add files via upload

---
+++ Laba2-2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace _2LB___2ZDN
+{
+    class Laba2_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String:");
+            string InputString1 = Console.ReadLine();
+            string[] Words = InputString1.Split(' ');
+            for (int i = Words.Length - 1; i >= 0; i--)
+            {
+                Console.Write(Words[i] + " ");
+            }
+        }
+    }
+}
